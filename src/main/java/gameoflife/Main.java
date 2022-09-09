6466d0c9343ed0e864e211834bb94e8fe664d5af
Fabio Niephaus <fabio.niephaus@oracle.com>
2022-09-09 09:14:20 +0200

    Switch to enhanced `ConsoleOutput`.

--- src/main/java/gameoflife/Main.java
+++ src/main/java/gameoflife/Main.java
@@ -22,10 +22,10 @@ public class Main {
                     args.length > 1 ? Integer.parseInt(args[1]) : 640,
                     args.length > 2 ? Integer.parseInt(args[2]) : 480,
                     args.length > 3 ? Integer.parseInt(args[3]) : 20,
-                    args.length > 4 ? Integer.parseInt(args[4]) : 10,
-                    args.length > 5 ? Integer.parseInt(args[5]) : 10,
-                    args.length > 6 ? Integer.parseInt(args[6]) : 40,
-                    args.length > 7 ? Integer.parseInt(args[7]) : 40,
+                    args.length > 4 ? Integer.parseInt(args[4]) : 2,
+                    args.length > 5 ? Integer.parseInt(args[5]) : 2,
+                    args.length > 6 ? Integer.parseInt(args[6]) : 20,
+                    args.length > 7 ? Integer.parseInt(args[7]) : 20,
                     args.length > 8 ? Boolean.parseBoolean(args[8]) : false,
                     args.length > 9 ? Boolean.parseBoolean(args[9]) : false);
         }
@@ -46,8 +46,8 @@ public class Main {
         double scale = calculateScale(dimensions.rows(), dimensions.cols(), a.maxWindowWidth, a.maxWindowHeight);
         var width = (int) (scale * dimensions.cols());
         var height = (int) (scale * dimensions.rows());
-        System.out.printf("rows=%d, columns=%d, width=%d, height=%d\n", dimensions.rows(), dimensions.cols(), width, height);
-        Consumer<boolean[][]> consumer = new CountingOutput();
+        // System.out.printf("rows=%d, columns=%d, width=%d, height=%d\n", dimensions.rows(), dimensions.cols(), width, height);
+        Consumer<boolean[][]> consumer = new ConsoleOutput();
 
         while (true) {
             consumer.accept(gridChannel.take());
